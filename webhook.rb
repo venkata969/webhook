6f313f53eb969d86e7a35d1c041bbd2c0aae8708
Ramesh <venkatarameshedara@gmail.com>
2023-01-07 01:36:11 +0000

    webook.rb

---
+++ webhook.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(:Port => ARGV.first)
+server.mount_proc '/' do |req, res|
+  puts req.body
+end
+
+trap 'INT' do
+  server.shutdown
+end
+server.start
+
+
+
+#ruby webhook.rb 8000
+#http://my.host:8000/
